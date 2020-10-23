fa14a70f293d921b28a58a9021ce0d5fbc832fa6
babyazalea <usun12@hotmail.com>
2020-10-23 23:27:57 +0900

    Updates

---
+++ precache-manifest.2e6b87c1a69efe65dc9094060fb5fd98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8155dbb7465e5eac87b2f154c3899fa",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "d2f56a2c39a2dbc6906c",
+    "url": "/nwitter/static/css/main.cba8b23c.chunk.css"
+  },
+  {
+    "revision": "a363b6fcb8c6e254a48f",
+    "url": "/nwitter/static/js/2.f1e90fb9.chunk.js"
+  },
+  {
+    "revision": "7e320732ce8e269e8d356159c55ade0e",
+    "url": "/nwitter/static/js/2.f1e90fb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2f56a2c39a2dbc6906c",
+    "url": "/nwitter/static/js/main.b69208d7.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
